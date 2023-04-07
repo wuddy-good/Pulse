5e2eccbdbf82a9d2354f6f8386e5ed25e7665bd1
Vlad Polishchuk <vladpolisuk127@gmail.com>
2023-04-07 23:16:59 +0300

    add map

---
+++ src/js/tiny-slider.js
@@ -0,0 +1,10 @@
+var slider = tns({
+    container: '.my-slider',
+    items: 1,
+    slideBy: 'page',
+    prevButton: ".prev",
+    nextButton: ".next",
+    nav: false,
+    autoHeight	: true,
+    center: true,
+});
